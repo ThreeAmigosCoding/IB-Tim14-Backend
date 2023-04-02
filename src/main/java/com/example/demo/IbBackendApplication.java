e071475dfe045e5342114c72859feb4981694284
Marko Janosevic <markojanosevicsm7@gmail.com>
2023-04-02 23:40:04 +0200

    Implemented login

--- src/main/java/com/example/demo/IbBackendApplication.java
+++ src/main/java/com/example/demo/IbBackendApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ObservationAuthenticationManager;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class IbBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IbBackendApplication.class, args);
 	}
--- src/main/java/com/example/demo/controller/user/UserController.java
+++ src/main/java/com/example/demo/controller/user/UserController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller.user;
 
 
+import com.example.demo.dto.ErrorDTO;
 import com.example.demo.dto.user.UserDTO;
+import com.example.demo.dto.user.UserTokenState;
 import com.example.demo.model.user.User;
 import com.example.demo.service.user.UserService;
+import com.example.demo.util.TokenUtils;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +23,34 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
+    private TokenUtils tokenUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
     private UserService userService;
+
+    @PostMapping(value = "/login", consumes = "application/json")
+    public ResponseEntity<?> login(@RequestBody UserDTO authenticationRequest) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getEmail(), authenticationRequest.getPassword()));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            User user = (User) authentication.getPrincipal();
+            String jwt = tokenUtils.generateToken(user);
+            int expiresIn = tokenUtils.getExpiredIn();
+
+            System.out.println(user.getAuthorities());
+
+            return ResponseEntity.ok(new UserTokenState(jwt, expiresIn));
+        } catch (Exception e) {
+            return new ResponseEntity<>(new ErrorDTO("Wrong username or password!"), HttpStatus.BAD_REQUEST);
+        }
+
+    }
 
     @PostMapping(value = "/register", consumes = "application/json")
     public ResponseEntity<?> register(@Valid @RequestBody UserDTO userDTO){
