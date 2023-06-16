ceadd7056f1bf1683db2baf567e7bc82b301b8b8
cuturic01 <miloscuturic8@gmail.com>
2023-06-16 21:31:14 +0200

    Updated gitignore

---
+++ src/main/java/com/example/demo/util/MyCredentials.java
@@ -0,0 +1,8 @@
+package com.example.demo.util;
+
+public class MyCredentials {
+    public static String reCaptchaKey = "placeholder";
+    public static String sendGridKey = "placeholder";
+    public static String oAuthClientId = "placeholder";
+    public static String oAuthClientSecret = "placeholder";
+}
